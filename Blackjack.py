c3fd453a35e3ad412e164a572de4b8191e5260a8
SEary342 <45857544+SEary342@users.noreply.github.com>
2019-07-26 11:53:41 -0400

    Create Blackjack.py

---
+++ Blackjack.py
@@ -0,0 +1,33 @@
+from Cards import build_deck
+
+GAME_DECK = build_deck()
+PLAYER_CARDS = []
+COMPUTER_CARDS = []
+
+
+def player_turn():
+    pass
+
+
+def computer_turn():
+    pass
+
+
+def game_loop():
+    pass
+
+
+def start_game():
+    pass
+
+
+def win_check() -> bool:
+    pass
+
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main()
