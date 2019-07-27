313ab354cfc33ba39647fd133827d3b68d0346db
SEary342 <45857544+SEary342@users.noreply.github.com>
2019-07-27 18:50:06 -0400

    working implementation and major refactor

--- Blackjack.py
+++ Blackjack.py
@@ -1,4 +1,4 @@
-from Cards import build_deck
+from Cards import build_deck, Card
 
 GAME_DECK = []
 PLAYER_CARDS = []
@@ -7,51 +7,88 @@ PLAYER = None
 
 
 class Player:
-    def __init__(self, money):
+    def __init__(self, money: int):
         self.money = money
 
-    def win_bet(self, bet):
+    def win_bet(self, bet: int):
         self.money = self.money + bet
 
-    def lose_bet(self, bet):
-        self.money = self.money + bet
+    def lose_bet(self, bet: int):
+        self.money = self.money - bet
 
     def get_money(self):
         return self.money
 
 
-def player_turn():
-    pass
+def main():
+    global PLAYER
+    PLAYER = Player(500)
+
+    while True:
+        print(f"You have ${PLAYER.get_money()}")
+        bet = bet_request()
+
+        win_loss = game()
+
+        if win_loss == "Win":
+            PLAYER.win_bet(bet)
+            status = "Won"
+        elif win_loss == "Push":
+            status = "Tied"
+        elif win_loss == "Loss":
+            PLAYER.lose_bet(bet)
+            status = "Lost"
+        else:
+            raise NotImplementedError
+
+        print(f"Game Over! You {status}!")
+        print(f"You have ${PLAYER.get_money()} remaining")
+
+        cont_game = ""
+        while cont_game not in ("y", "n"):
+            cont_game = input(f"Play again? Y/N ").lower()
+        if cont_game == "n":
+            break
+        elif cont_game == "y":
+            cont_game == ""
+            continue
+        else:
+            raise NotImplementedError
 
 
-def computer_turn():
-    pass
+def bet_request():
+    while True:
+        input_val = input("How much would you like to bet? ")
+        try:
+            bet_amt = int(input_val)
+            if bet_amt > 0 and bet_amt <= PLAYER.get_money():
+                return bet_amt
+            elif bet_amt > PLAYER.get_money():
+                raise Exception
+            else:
+                raise ValueError
+
+        except Exception:
+            print(
+                "Please make a bet less than or equal to your current amount of money."
+            )
+            print(f"You currently have ${PLAYER.get_money()}")
+            continue
+
+        except ValueError:
+            print(
+                f"You entered '{input_val}'. This is not a valid positive whole number."
+            )
+            print("Please try again.")
+            continue
 
 
-def game_loop():
+def game():
     initialize_game()
     initial_deal()
-
-    # Returns player win status
-    return True
-
-
-def turn_loop():
-    pass
-
-
-def initial_deal():
-    for num in range(0, 2):
-        PLAYER_CARDS.append(GAME_DECK.pop(0))
-        DEALER_CARDS.append(GAME_DECK.pop(0))
-
-
-def deal():
-    pass
-
-
-def win_check() -> bool:
-    pass
+    player_score = player_turn()
+    dealer_score = dealer_turn(player_score)
+    return win_check(player_score, dealer_score)
 
 
 def initialize_game():
@@ -61,29 +98,71 @@ def initialize_game():
     DEALER_CARDS = []
 
 
-def main():
-    global PLAYER
-    PLAYER = Player(500)
+def initial_deal():
+    for num in range(0, 2):
+        deal("D")
+        deal("P")
 
-    while True:
-        print(f"You have ${PLAYER.get_money}")
-        print(f"How much would you like to bet?")
-        # TODO: implement input system here
-        bet = 20
-        win_loss = game_loop()
 
-        if win_loss:
-            PLAYER.win_bet(bet)
-            status = "Won"
+def player_turn():
+    while over_check(PLAYER_CARDS, 21):
+        print(f"Dealer shows: {Card.string_cards(DEALER_CARDS, True)}")
+        print(f"You have: {Card.string_cards(PLAYER_CARDS)}")
+        hit_status = ""
+        while hit_status not in ("y", "n"):
+            hit_status = input(f"Hit? Y/N ").lower()
+        if hit_status == "y":
+            deal("P")
         else:
-            PLAYER.lose_bet(bet)
-            status = "Lost"
+            break
+    return Card.cards_value(PLAYER_CARDS)
 
-        print(f"Game Over! You {status}!")
-        print(f"You have ${PLAYER.get_money} remaining")
-        print(f"Play again? Y/N")
-        # TODO: implement input system here
-        break
+
+def dealer_turn(player_score: int):
+    print(f"Dealer shows: {Card.string_cards(DEALER_CARDS)}")
+    min_score = 17
+    if player_score < 17:
+        min_score = player_score
+    while over_check(DEALER_CARDS, min_score):
+        deal("D")
+        print(f"Dealer shows: {Card.string_cards(DEALER_CARDS)}")
+    return Card.cards_value(DEALER_CARDS)
+
+
+def over_check(card_list: int, threshold_num: int) -> bool:
+    return Card.cards_value(card_list) <= threshold_num
+
+
+def deal(destination: str):
+    if destination == "P":
+        PLAYER_CARDS.append(GAME_DECK.pop(0))
+    elif destination == "D":
+        DEALER_CARDS.append(GAME_DECK.pop(0))
+    else:
+        raise NotImplementedError
+
+
+def win_check(player_score: int, dealer_score: int) -> str:
+    if player_score > 21:
+        return "Loss"
+    else:
+        if dealer_score > 21:
+            return "Win"
+        elif player_score > dealer_score:
+            return "Win"
+        elif player_score == dealer_score:
+            return "Push"
+        else:
+            return "Loss"
+
+
+def get_decks(type: str):
+    if type == "G":
+        return GAME_DECK
+    elif type == "P":
+        return PLAYER_CARDS
+    elif type == "D":
+        return DEALER_CARDS
 
 
 if __name__ == "__main__":
--- Cards.py
+++ Cards.py
@@ -22,23 +22,55 @@ class Card:
     def __str__(self):
         return f"{self.rank} of {self.suit}"
 
+    @staticmethod
+    def string_cards(card_list, exclude_first=False, print_value=True):
+        card_string = ""
+        for i, card in enumerate(card_list):
+            if exclude_first and i == 0:
+                card_string = "(Hidden), "
+                continue
+            card_string = card_string + str(card) + ", "
+        card_string = card_string[: len(card_string) - 2]
+
+        if print_value:
+            display_total = Card.cards_value(card_list)
+            if exclude_first:
+                display_total -= card_list[0].value
+            card_string = card_string + f" (Total: {display_total})"
+
+        return card_string
+
+    @staticmethod
+    def cards_value(card_list):
+        ace_1 = None
+        ace_11 = None
+        card_sum = 0
+        for card in card_list:
+            if type(card.value) == tuple:
+                ace_1 = card.value[0]
+                ace_11 = card.value[1]
+            else:
+                card_sum = card_sum + card.value
+        if ace_1 is not None:
+            ace_1_sum = card_sum + ace_1
+            ace_11_sum = card_sum + ace_11
+
+            if ace_11_sum > 21:
+                return ace_1_sum
+            else:
+                return ace_11_sum
+        else:
+            return card_sum
+
 
 def build_deck():
     deck = []
     for suit in SUITS:
         for rank in RANKS:
             deck.append(Card(rank, suit, RANK_VAL[rank]))
-    return shuffle(deck)
+    return _shuffle(deck)
 
 
-def shuffle(deck):
+def _shuffle(deck):
     random.shuffle(deck)
     return deck
-
-
-if __name__ == "__main__":
-    deck = build_deck()
-    print(deck)
-    for item in deck:
-        print(str(item))
-    print(len(deck))
